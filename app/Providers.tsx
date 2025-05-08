9a5c51cee2092a589acd3660282c282469830782
Maragada Srinu <srinumaragada@gmail.com>
2025-05-08 22:08:56 +0530

    Add Providers component

---
+++ app/Providers.tsx
@@ -0,0 +1,10 @@
+// app/providers.tsx
+'use client';
+
+
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
+}
