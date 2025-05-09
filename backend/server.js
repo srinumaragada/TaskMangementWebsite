ff6d69534753a06de7ae6dce1f8cf2962528f9e1
Maragada Srinu <srinumaragada@gmail.com>
2025-05-10 04:03:16 +0530

    add url in server for socket

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000",process.env.FRONTEND_URL],
+    origin: ["http://localhost:3000","https://task-mangement-website.vercel.app/"],
     methods: ["GET", "POST"],
     credentials: true
   },
