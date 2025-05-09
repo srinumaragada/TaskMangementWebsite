8cc9161ebd03f42f3ab6f47648078676be8c528d
Maragada Srinu <srinumaragada@gmail.com>
2025-05-10 02:24:39 +0530

    Update global layout

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import { GeistSans, GeistMono } from "geist/font";
 import "./globals.css";
 import ClientLayout from "./clientLayout";
 import NotificationListener from "./components/NotificationListener";
@@ -23,7 +22,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable} ${inter.variable}`}>
+    <html lang="en" className={` ${inter.variable}`}>
       <body className="antialiased">
         <ClientLayout>
           <NotificationListener />
