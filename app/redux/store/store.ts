a695c30511fbe66cfb99a3d26b5ca39f77df6c2b
Maragada Srinu <srinumaragada@gmail.com>
2025-05-09 17:44:55 +0530

    Configure redux store with new slices and middleware

--- app/redux/store/store.ts
+++ app/redux/store/store.ts
@@ -4,14 +4,23 @@ import authReducer from "../slice/UserSlice";
 import TaskReducer from "../slice/TaskSlice"
 import projectsReducer from "../slice/projects/projectsSlice"
 import tasksReducer from "../slice/projects/taskSlice"
+import notificationReducer from "../slice/notificationSlice"
 export const store = configureStore({
   reducer: {
     Auth: authReducer,
     Tasks:TaskReducer,
     projects: projectsReducer,
-    Projecttasks: tasksReducer
-  }
-  
+    Projecttasks: tasksReducer,
+    notification: notificationReducer,
+  },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ['payload.data'],
+      },
+    }),
+
 });
 
 
