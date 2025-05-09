449b269626925180ade65d9e34e99a1e0e735e89
Maragada Srinu <srinumaragada@gmail.com>
2025-05-10 02:24:40 +0530

    Update landing page

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,44 +1,12 @@
-// Home.tsx or HomePage.tsx
-"use client";
 
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { AppDispatch, RootState } from "./redux/store/store";
-import { checkAuth } from "./redux/slice/UserSlice";
-import CheckAuth from "./components/CheckAuth";
 import HomePage from "./components/HomePage";
+
 export default function Home() {
-  const { user, isAuthenticated, isLoading } = useSelector(
-    (state: RootState) => state.Auth
-  );
-  console.log(user?.id);
-    
-  const dispatch = useDispatch<AppDispatch>();
-  const router = useRouter();
 
-  useEffect(() => {
-    dispatch(checkAuth());
-  }, [dispatch]);
 
-  // Handle redirection if not authenticated
-  useEffect(() => {
-    if (isLoading) return; // Wait for loading to finish
-    if (!isAuthenticated) {
-      router.push("/");
-    }
-  }, [isAuthenticated, isLoading, router]);
-
-  
   return (
-    <div>
-      {/* Protected route */}
-      <CheckAuth isAuthenticated={isAuthenticated} >
-        <HomePage />
-      </CheckAuth>
+  
+      <HomePage />
 
-     
-    </div>
   );
 }
